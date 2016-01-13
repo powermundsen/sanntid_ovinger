787295f4dbc2e2d800e0a3bedd524bc2f62ff905
powermundsen <awe992@gmail.com>
2016-01-13 13:33:19 +0100

    Oppdatering av alle tingene

---
+++ sanntid_ovinger/c-fil.c
@@ -0,0 +1,9 @@
+//
+//  c-fil.c
+//  sanntid_ovinger
+//
+//  Created by Andre Nydegger Wermundsen on 13.01.2016.
+//  Copyright © 2016 Andre Nydegger Wermundsen. All rights reserved.
+//
+
+#include "c-fil.h"
---
+++ sanntid_ovinger/c-fil.h
@@ -0,0 +1,14 @@
+//
+//  c-fil.h
+//  sanntid_ovinger
+//
+//  Created by Andre Nydegger Wermundsen on 13.01.2016.
+//  Copyright © 2016 Andre Nydegger Wermundsen. All rights reserved.
+//
+
+#ifndef c_fil_h
+#define c_fil_h
+
+#include <stdio.h>
+
+#endif /* c_fil_h */
